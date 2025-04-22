1b0ed6cca530eb378b1bfa7b07b5ed8952f26596
Mzrbt <jabetmarin07@gmail.com>
2025-04-22 14:07:37 +0200

    Exo 1.4

--- tp3/tp3/src/main/java/iut/gon/tp3/GrilleController.java
+++ tp3/tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -5,14 +5,33 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GrilleController implements Initializable{
 	
 	private @FXML GridPane grille;
+	
+	private Label[][] labels = new Label[3][3];
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		grille.setStyle("-fx-background-color: seashell");		
+		grille.setStyle("-fx-background-color: seashell");
+		for (int lg = 0; lg < 3; lg++) {
+            for (int col = 0; col < 3; col++) {
+                Label label = new Label(String.format("L%dC%d", lg, col));
+                labels[lg][col] = label;
+                grille.add(label, col, lg);
+                
+                label.setOnMouseClicked(event -> {
+                	label.setText("Bonjour");
+                });
+                
+                label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                
+                label.setAlignment(javafx.geometry.Pos.CENTER);
+                label.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+            }
+        }
 	}
 }
